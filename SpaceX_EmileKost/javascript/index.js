086e4f5833cea9447a508aa2199db825cee466fe
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2023-05-24 10:08:39 +0200

    Add files via upload

--- SpaceX_EmileKost/javascript/index.js
+++ SpaceX_EmileKost/javascript/index.js
@@ -7,6 +7,20 @@ button.addEventListener('click', () => {
     location.reload();
 })
 
-spacex.addEventListener('mouseover', (event) => {
-    console.log('Hoverttt', event)
-})
+const key = 'BSmwdny3eqvIym0Cz56IAnyE4e45vbTdmgAv0Z2q';
+const weatherURL ='https://api.openweathermap.org/data/2.5/weather?q=olmito&appid=3b4a4ee478f17695c6ad24052431fd68';
+let weatherParagraph = document.querySelector('p');
+
+function fetchMarsWeather(url) {
+    fetch(url)
+    .then(async response => {
+        const data  = await response.json();
+        console.log(data);
+        weatherParagraph.innerText = `Weather Launch Site: ${data.wind.deg} degrees, ${data.weather[0].main}`
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+fetchMarsWeather(weatherURL)
